d76613cb97747883a22843297ebeb2b69250fa00
snu-urim <woorjb133@gmail.com>
2020-02-19 00:37:44 -0500

    changed image PATH

--- bg.js
+++ bg.js
@@ -8,7 +8,7 @@ function handelImgLoad(){
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `/images/${imgNumber}.jpg`;
+    image.src = `./images/${imgNumber}.jpg`;
     image.classList.add('bgImg');
     body.prepend(image);
 }
